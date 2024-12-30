0f092b13a18f835beab83442a74b730bd293b2ea
prazzdev <prazzid31@gmail.com>
2024-12-31 01:51:09 +0700

    added contentful, configuring it for project list, added project icons.

--- src/components/ProjectBox/index.jsx
+++ src/components/ProjectBox/index.jsx
@@ -1,14 +1,7 @@
 // import Image from "next/image"
 
 export default function ProjectBox(props) {
-  const {
-    title,
-    thumbUrl = "btw",
-    desc,
-    repoUrl,
-    projectUrl,
-    children,
-  } = props;
+  const { title, thumbUrl, desc, repoUrl, projectUrl, children } = props;
   const repoUri = "https://github.com/prazzdev/";
   const imgUri = "/images/projects/";
   return (
@@ -16,7 +9,7 @@ export default function ProjectBox(props) {
       <div className="sm:max-w-[100%] lg:min-w-[28vw] lg:max-w-[28vw] 2xl:min-w-[18vw] 2xl:max-w-[18vw] min-h-fit lg:mr-5 mb-5 flex flex-col px-6 py-6 rounded-2xl overflow-hidden bg-white shadow-md hover:bg-gray-100 hover:-translate-y-2 hover:shadow-lg transition">
         <div id="head" className="flex flex-col justify-between mb-4">
           <img
-            src={imgUri + thumbUrl + ".webp"}
+            src={thumbUrl}
             width={410}
             height={300}
             loading="lazy"
@@ -28,7 +21,7 @@ export default function ProjectBox(props) {
             </h3>
             <div id="opt" className="max-w-[45%] lg:max-w-[30%]">
               <button className="mx-2">
-                <a href={repoUri + repoUrl} className="" target="_blank">
+                <a href={repoUrl} className="" target="_blank">
                   <img
                     src={"/images/icons/fork.svg"}
                     className="w-[20px] lg:w-[1.1rem]"
@@ -36,7 +29,7 @@ export default function ProjectBox(props) {
                 </a>
               </button>
               <button className="mx-2">
-                <a href={"https://" + projectUrl} className="" target="_blank">
+                <a href={projectUrl} className="" target="_blank">
                   <img
                     src={"/images/icons/direct.svg"}
                     className="w-[20px] lg:w-[1.1rem]"
@@ -46,7 +39,7 @@ export default function ProjectBox(props) {
             </div>
           </div>
         </div>
-        <div id="desc" className="mb-5">
+        <div id="desc" className="mb-5 min-h-[125px]">
           <span className="">{desc}</span>
         </div>
         <div id="tech" className="flex flex-row items-center">
